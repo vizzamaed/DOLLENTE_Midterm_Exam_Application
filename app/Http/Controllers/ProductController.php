aca2164b8325d37cd020d68e74326ef25efb4e12
ajmmosawan@gmail.com <vizzamaedollente@gmail.cogit config --global user.email vizzamaedollente@gmail.com>
2024-02-21 12:44:25 +0800

    DOLLENTE_Midterm_Exam_Application

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    function ProductController(
+        "m1"=>"The Hows of Us"
+        "m2"=>"Rewind"
+    )
+}
---
+++ resources/views/movies.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Movies</title>
+</head>
+<body>
+    <p>Watch trending movies today!</p>
+    <p>#1</p>
+    <p>{{m1}}</p>
+    <p>#2</p>
+    <p>{{m2}}</p>
+    <p>#3</p>
+    <p>{{m3}}</p>
+</body>
+</html>
